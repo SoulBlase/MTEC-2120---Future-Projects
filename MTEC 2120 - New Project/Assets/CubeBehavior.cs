b077ee45be545ffd771a6a897d4035d7feab2d83
Quincy Lewis <Quincy.Lewis@mail.citytech.cuny.edu>
2019-09-11 11:39:37 -0400

    Modified Unity file

---
+++ MTEC 2120 - New Project/Assets/CubeBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehavior : MonoBehaviour
+{
+    public float Number;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Number = -5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MTEC 2120 - New Project/Assets/WallBuilderBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBuilderBehavior : MonoBehaviour
+{
+    public GameObject CubePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(CubePrefab, new Vector3(0, 5, 9), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+}
