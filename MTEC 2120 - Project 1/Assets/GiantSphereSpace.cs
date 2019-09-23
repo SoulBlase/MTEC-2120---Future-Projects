416a3c1e3c3b253333c5772a85d3e01bb1eb02dc
Quincy Lewis <Quincy.Lewis@mail.citytech.cuny.edu>
2019-09-23 13:24:00 -0400

    Added Unity File

---
+++ MTEC 2120 - Project 1/Assets/GiantSphereSpace.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiantSphereSpace : MonoBehaviour
+{
+    public Vector3 LookAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //widen the sphere
+        transform.localScale += new Vector3(50, 50, 50);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Mouse X") != 0)
+        {
+            
+        }
+    }
+}
