65a6fbb14f0df1988b7b3c2648a1ec5f10faa41c
Quincy Lewis <Quincy.Lewis@mail.citytech.cuny.edu>
2019-10-16 10:58:45 -0400

    Added Scripts

---
+++ Unity Homework Assignment #2/Assets/CubeBehaviorScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviorScript : MonoBehaviour
+{
+    public GameObject SomeObject;
+    Rigidbody SomeRigidbody;
+    Vector3 PositionOffset;
+
+    private bool correct;
+    private bool My_Play;
+
+    AudioSource My_AudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PositionOffset = SomeObject.transform.position;
+        My_AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SomeObject == correct)
+        {
+            SomeObject.transform.position = new Vector3(0, Mathf.Sin(Time.time) * 10, 0) + PositionOffset;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                My_Play = true;
+                My_AudioSource.Play();
+            }
+            
+        }
+        else
+        {
+            My_Play = false;
+            My_AudioSource.Stop();
+        }
+    }
+}
